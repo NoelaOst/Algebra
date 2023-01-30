df2b6b09fdf8e1eb6bd10bde6d1d55da1eada6d4
Noela Ost <noela.ostric@gmail.com>
2023-01-30 21:27:28 +0100

    change on ispit js

---
+++ domaci-radovi/Parcijalni ispit/ispit-js-main.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#input");
+const resultList = document.querySelector('#result-list');
+
+const handleSearchChange = () => {
+    const userInput = input.value;
+    if (userInput.length < 3) {
+        return
+    }
+    const url = `https://itunes.apple.com/search?term=${userInput}&entity=musicArtist`;
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onreadystatechange = () => {
+        if(request.status === 200){
+            const responseObject = JSON.parse(request.response);
+            const artistName = responseObject.artistName;
+            resultList.innerHTML = " ";
+
+            for (let i = 0; i < artistName.resultCount; i++) {
+                let item = artistName.results[i];
+                let li = document.createElement("li");
+                li.innerHTML = item;
+                resultList.appendChild(li);
+              }
+        }
+
+    }
+    request.send();
+}
+
+input.addEventListener("input", handleSearchChange);
