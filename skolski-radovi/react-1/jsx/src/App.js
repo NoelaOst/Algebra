949b14522dd3c64152e7bae608bbc4d2c5be168d
Noela Ost <noela.ostric@gmail.com>
2023-02-17 19:31:07 +0100

    new changes

---
+++ skolski-radovi/react-1/jsx/src/App.js
@@ -0,0 +1,12 @@
+import Komponenta1 from './komponenta';
+
+function App() {
+  return (
+      <div>
+        <h1>Ovo je test</h1>
+        <Komponenta1/>
+      </div>
+
+  );
+}
+export default App;
---
+++ skolski-radovi/react-1/jsx/src/komponenta.js
@@ -0,0 +1,7 @@
+function Komponenta1 () {
+    return (
+        <p>Ovo je Komponenta 1</p>
+    )
+}
+
+export default Komponenta1;
